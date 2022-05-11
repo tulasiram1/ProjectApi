e3b788ef172e2ecdeccf7aaef4b932f41e0fee7a
Tulasi Ram Reddy <tulasir287@gmail.com>
2022-05-11 12:51:02 +0530

    commit

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const controller = require('./controller/controller');
 const morgan = require('morgan');
+const compression = require('compression')
 
 dotenv.config({ path: './config.env' });
 app.use(morgan('dev'))
+app.use(compression);
 app.listen(process.env.PORT, () => { console.log(`listening to port number ${process.env.PORT}`); });
 const URL = process.env.DATABASE_URL.replace('<password>', process.env.DATABASE);
 mongoose.connect(URL).then(() => { console.log('connection success'); }).catch((err) => { console.log(err); });
